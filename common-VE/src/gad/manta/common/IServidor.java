a4560049426e7b8fc65c281e7b4cd0502d0d3e67
AntKevLazArt <laz-anthony1996@hotmail.com>
2017-10-12 16:45:25 -0500

    holi

--- common-VE/src/gad/manta/common/IServidor.java
+++
@@ -1,9 +0,0 @@
-package gad.manta.common;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IServidor extends Remote {
-	public String saludar(String nombre) throws RemoteException;
-
-}
