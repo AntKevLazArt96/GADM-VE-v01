d65c9945dfc47aea832267e20ae185b74f0f524a
AntKevLazArt <laz-anthony1996@hotmail.com>
2017-10-12 16:41:08 -0500

    V 0.1

---
+++ servidor-VE/src/gad/manta/servidor/Servidor.java
@@ -0,0 +1,16 @@
+package gad.manta.servidor;
+
+import java.rmi.RemoteException;
+
+import gad.manta.common.IServidor;
+
+public class Servidor implements IServidor {
+
+	@Override
+	public String saludar(String nombre) throws RemoteException {
+		// TODO Auto-generated method stub
+		System.out.println("Tu nombre es: "+ nombre);
+		return "Hola tu nombre es "+ nombre;
+	}
+
+}
