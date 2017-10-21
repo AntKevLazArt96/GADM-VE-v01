42df97b0e1e0e2b42be50814f8290d6fa60b7c3a
AntKevLazArt <laz-anthony1996@hotmail.com>
2017-10-21 15:38:19 -0500

    Pruebas a interfaz

--- Prueba1/src/application/Main.java
+++ Prueba1/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +18,7 @@ public class Main extends Application {
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			//primaryStage.initStyle(StageStyle.UNDECORATED);
 			primaryStage.show();
 			
 			
