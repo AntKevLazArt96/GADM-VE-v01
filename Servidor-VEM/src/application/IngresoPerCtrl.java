119bc1722e5a4a2c8359ae440ee33fb82190145e
chris <gonpina94@hotmail.com>
2017-12-02 12:22:03 -0500

    VALIDACION INGRESO DE USUARIOS
    EN ESTA PARTE ESTA HECHA TODAS LAS VALIDACIONES DEL INGRESO DEL USUARIO

--- Servidor-VEM/src/application/IngresoPerCtrl.java
+++ Servidor-VEM/src/application/IngresoPerCtrl.java
@@ -4,6 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
@@ -15,10 +20,16 @@ import gad.manta.common.Imagen;
 import gad.manta.common.Usuario;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.FileChooser.ExtensionFilter;
 
 
@@ -99,19 +110,63 @@ public class IngresoPerCtrl implements Initializable {
     }*/
     
     @FXML
-    void onGuardar(ActionEvent event) {
+    void onGuardar(ActionEvent event) throws SQLException {
     	
     	
     	try {
-    		conexion.establecerConexion();
-    		Usuario user = new Usuario(txt_cedula.getText(),txt_cargo.getText(),txt_nombre.getText(),txt_username.getText(),txt_password.getText(),id_img);
-    		int resultado = user.guardarRegistro(conexion.getConnection());
-    		if(resultado ==1) {
-    			//limpiar();
-    			imgBlanco();
-    		}else{
-    			System.out.println("hay ocurrido algun error");
-    		}
+    		Connection db;
+			db = DriverManager.getConnection("jdbc:postgresql:gad_voto","postgres","1234");
+			Statement st = db.createStatement();
+			ResultSet resultado1= st.executeQuery("select * from consulta_usuario_ced('"+txt_cedula.getText()+"');");
+			db.close();
+			conexion.establecerConexion();
+    		
+    		if(txt_cedula.getLength()==0) {
+    			txt_cedula.requestFocus();
+    			mostrarMesaje("Falta ingresar el número de cédula");
+    			
+    		}else
+    			if(resultado1.next()) {
+        			txt_cedula.requestFocus();
+        			mostrarMesaje("El usuario con cédula "+txt_cedula.getText()+ " ya está registrado en el sistema");
+        			
+        		}else 
+            
+        		if(txt_nombre.getLength()==0) {
+        			txt_nombre.requestFocus();
+        			mostrarMesaje("Falta ingresar el nombre del usuario");
+        		}else 
+            		if(txt_username.getLength()==0) {
+            			txt_username.requestFocus();
+            			mostrarMesaje("Falta ingresar el nombre de usuario");
+            		}else 
+            			if(txt_password.getLength()==0) {
+            				txt_password.requestFocus();
+            				mostrarMesaje("Falta ingresar la contraceña del usuario");
+            			}else 
+            				if(txt_cargo.getLength()==0) {
+            					txt_cargo.requestFocus();
+            					mostrarMesaje("Falta ingresar el cargo del usuario");
+            				}else if(id_img==0) {
+            					btn_examinar.requestFocus();
+            					mostrarMesaje("Falta ingresar la foto del usuario");
+            				}else {
+            					
+            		  						
+                    			Usuario user = new Usuario(txt_cedula.getText(),txt_cargo.getText(),txt_nombre.getText(),txt_username.getText(),txt_password.getText(),id_img);
+                        		int resultado = user.guardarRegistro(conexion.getConnection());                       		
+                        		
+                        		if(resultado ==1) {
+                        			//limpiar();
+                        			imgBlanco();
+                        			mostrarMesaje("El usuario "+txt_nombre.getText()+" a sido ingresado correctamente");
+                        		}else{
+                        			mostrarMesaje("El usuario "+txt_nombre.getText()+" no se a podido agregar");
+                        		}
+                        		
+                    		}
+    		
+    		
     		
     		conexion.cerrarConexion();
     		
@@ -123,7 +178,25 @@ public class IngresoPerCtrl implements Initializable {
 		}
 		
     }
+    public void mostrarMesaje(String subtitulo) {
+		
+		try {
+			data.header = "Aviso";
+			data.cuerpo = subtitulo;
+			
+			Stage newStage = new Stage();
+			AnchorPane pane;
+			pane = (AnchorPane) FXMLLoader.load(getClass().getResource("VentanaDialogo.fxml"));
+			Scene scene = new Scene(pane);
+			newStage.setScene(scene);
+			newStage.initStyle(StageStyle.UNDECORATED);
+			newStage.show();
 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		txt_nombre.setText("AB. JOS� PICO ARTEAGA");
@@ -164,5 +237,80 @@ public class IngresoPerCtrl implements Initializable {
 			System.out.println("Error en la consulta");
 		}
 	}
-
+	@FXML
+	void  validar_cedula(KeyEvent e) {
+		
+		char car = e.getCharacter().charAt(0);
+		if(!Character.isDigit(car)) {
+			e.consume();
+		}
+		if(txt_cedula.getLength()==10) {
+			e.consume();
+		}
+			
+	}
+	
+	@FXML
+	void  validar_nombre(KeyEvent e) {
+		
+		char car = e.getCharacter().charAt(0);
+		if(!(Character.isDigit(car)|| Character.isLetter(car)|| Character.isSpaceChar(car))) {
+			e.consume();
+		}
+		if(txt_nombre.getLength()==100) {
+			e.consume();
+		}
+		
+	}
+	
+	@FXML
+	void  validar_usuario(KeyEvent e) {
+		
+		char car = e.getCharacter().charAt(0);
+		if(!(Character.isDigit(car)|| Character.isLetter(car))) {
+			e.consume();
+		}
+		if(txt_username.getLength()==30) {
+			e.consume();
+		}
+		
+		
+	}
+	
+	@FXML
+	void  validar_contraceña(KeyEvent e) {
+		
+		char car = e.getCharacter().charAt(0);
+		if(!(Character.isDigit(car) || Character.isLetter(car))) {
+			e.consume();
+		}
+		if(txt_password.getLength()==30) {
+			e.consume();
+		}
+		
+		
+	}
+	
+	void  validar_foto() {
+		
+		if(id_img !=0) {
+			System.out.println("flata foto");
+		}
+		
+		
+	}
+	
+	@FXML
+	void  validar_cargo(KeyEvent e) {
+		
+		char car = e.getCharacter().charAt(0);
+		if(!(Character.isDigit(car) || Character.isLetter(car))) {
+			e.consume();
+		}
+		if(txt_cargo.getLength()==15) {
+			e.consume();
+		}
+		
+		
+	}
 }
