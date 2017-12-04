4ca48029a4de93bcf2b3f0223c07b65c125c308b
chris <gonpina94@hotmail.com>
2017-12-03 21:31:31 -0500

    parte de configuracion

---
+++ Servidor-VEM/src/application/ConfiguracionCtrl.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ConfiguracionCtrl implements Initializable {
+
+	public ConfiguracionCtrl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
